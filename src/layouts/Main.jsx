4d3d9d04573065f0bc11dcbc1b2f4b5362a71c6c
shejanNizam <nizam.shejan11@gmail.com>
2023-05-18 13:18:42 +0600

    simple project setup

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../pages/Shared/Footer/Footer";
+import NavBar from "../pages/Shared/NavBar/NavBar";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </>
+  );
 };
 
 export default Main;
